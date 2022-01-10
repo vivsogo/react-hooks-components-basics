ec145c5bf53ed0a0e67645b339c61156c6bdd859
Vivian Gomez <viviangomez0@gmail.com>
2022-01-09 20:51:34 -0500

    complete

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
 
+function Article(){
+  return (
+    <div>Dear Reader:Bjarne Stroustrup has the perfect lecture oration. </div>
+  );
+}
+
+function Comment() {
+  return <div>Naturally, I agree with this article.</div>;
+}
+
+
 function App() {
   return (
     <div>
